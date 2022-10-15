7abf2162e640b7a57e813e084c32702f1390b836
RevieNurArsena <revienr72@gmail.com>
2022-10-15 17:28:56 +0700

    Commit -4

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function ($) {   
+
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    $('.featured__controls li').on('click', function () {
+        $('.featured__controls li').removeClass('active');
+        $(this).addClass('active');
+    });
+    if ($('.featured__filter').length > 0) {
+        var containerEl = document.querySelector('.featured__filter');
+        var mixer = mixitup(containerEl);
+    }
+    
+    $(".categories__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: true,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+
+            0: {
+                items: 1,
+            },
+
+            480: {
+                items: 2,
+            },
+
+            768: {
+                items: 3,
+            },
+
+            992: {
+                items: 4,
+            }
+        }
+    });    
+
+})(jQuery);
